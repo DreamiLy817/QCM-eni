8e15f38f45424affc61ebbbbbcd8581fb4a193e4
DEOLIVEIRA MILHO <milho.deoliveira2017@campus-eni.fr>
2018-10-09 17:18:05 +0200

    UtilisateurDAOImpl

--- AMEL/src/main/java/fr/eni/projet/amel/dal/dao/UtilisateurDao.java
+++ AMEL/src/main/java/fr/eni/projet/amel/dal/dao/UtilisateurDao.java
@@ -1,5 +1,9 @@
 package fr.eni.projet.amel.dal.dao;
 
-public class UtilisateurDao {
+import fr.eni.projet.amel.bo.Utilisateur;
+import fr.eni.tp.web.common.dal.dao.GenericDAO;
 
+public interface UtilisateurDao extends GenericDAO<Utilisateur, Integer> {
+	
+	
 }
