87e8105d5a45152bcd586b6116f4c9806665e913
Leslie Baltimore <baltimore.leslie@gmail.com>
2018-10-09 15:16:43 +0200

    creation BO theme

---
+++ AMEL/src/main/java/fr/eni/projet/amel/bo/Theme.java
@@ -0,0 +1,48 @@
+package fr.eni.projet.amel.bo;
+
+import java.util.List;
+
+public class Theme {
+	
+	private int idTheme; 
+	private String libelle;
+	private List listeQuestions;
+	
+	//getter and setter 
+	public int getIdTheme() {
+		return idTheme;
+	}
+	public void setIdTheme(int idTheme) {
+		this.idTheme = idTheme;
+	}
+	public String getLibelle() {
+		return libelle;
+	}
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+	public List getListeQuestions() {
+		return listeQuestions;
+	}
+	public void setListeQuestions(List listeQuestions) {
+		this.listeQuestions = listeQuestions;
+	}
+	
+	//constructeur 
+	/**
+	 * @param idTheme
+	 * @param libelle
+	 * @param listeQuestions
+	 */
+	public Theme(int idTheme, String libelle, List listeQuestions) {
+		super();
+		setIdTheme(idTheme);
+		setLibelle(libelle);
+		setListeQuestions(listeQuestions);
+	}
+	
+	
+	
+	
+
+}
