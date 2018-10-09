13652470ce3f2d46656a598cb2f8cac128039e79
egrapin2017 <egrapin2017@21-UC36-06.ad.campus-eni.fr>
2018-10-09 17:07:10 +0200

    DAO proposition

--- AMEL/src/main/java/fr/eni/projet/amel/dal/dao/impl/QuestionDaoImpl.java
+++ AMEL/src/main/java/fr/eni/projet/amel/dal/dao/impl/QuestionDaoImpl.java
@@ -5,10 +5,14 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
+import fr.eni.projet.amel.bo.Proposition;
 import fr.eni.projet.amel.bo.Question;
+import fr.eni.projet.amel.dal.dao.PropositionDao;
 import fr.eni.projet.amel.dal.dao.QuestionDao;
+import fr.eni.projet.amel.dal.factory.DaoFactory;
 import fr.eni.tp.web.common.dal.exception.DaoException;
 import fr.eni.tp.web.common.dal.factory.MSSQLConnectionFactory;
 import fr.eni.tp.web.common.util.ResourceUtil;
@@ -32,30 +36,29 @@ public class QuestionDaoImpl implements QuestionDao {
 
 	@Override
 	public Question insert(Question question) throws DaoException {
-		Connection connection= null;
+		Connection connection = null;
 		PreparedStatement statement = null;
-        ResultSet resultSet = null;
-        
-        try{
-        	connection = MSSQLConnectionFactory.get();
-        	statement = connection.prepareStatement(INSERT_QUESTION_QUERY, Statement.RETURN_GENERATED_KEYS);
-        	statement.setString(1, question.getEnonce());
-        	statement.setString(2, question.getMedia());
-        	statement.setLong(3, question.getPoints());
-        	statement.setInt(4, idTheme);
-        	if(statement.executeUpdate()==1){
-        	resultSet = statement.getGeneratedKeys();
-        		if(resultSet.next()){
-        		question.setIdQuestion(resultSet.getInt(1));
-        		}
-        	}
-        }
-        catch(SQLException e){
-        	throw new DaoException(e.getMessage(), e);
-        }
-        finally{ResourceUtil.safeClose(resultSet, statement, connection);
-        }
-        
+		ResultSet resultSet = null;
+
+		try {
+			connection = MSSQLConnectionFactory.get();
+			statement = connection.prepareStatement(INSERT_QUESTION_QUERY, Statement.RETURN_GENERATED_KEYS);
+			statement.setString(1, question.getEnonce());
+			statement.setString(2, question.getMedia());
+			statement.setLong(3, question.getPoints());
+			statement.setInt(4, idTheme);
+			if (statement.executeUpdate() == 1) {
+				resultSet = statement.getGeneratedKeys();
+				if (resultSet.next()) {
+					question.setIdQuestion(resultSet.getInt(1));
+				}
+			}
+		} catch (SQLException e) {
+			throw new DaoException(e.getMessage(), e);
+		} finally {
+			ResourceUtil.safeClose(resultSet, statement, connection);
+		}
+
 		return question;
 	}
 
@@ -64,48 +67,110 @@ public class QuestionDaoImpl implements QuestionDao {
 		Connection connection = null;
 		PreparedStatement statement = null;
 		ResultSet resultSet = null;
-		try{
+		try {
 			connection = MSSQLConnectionFactory.get();
-			
-		}catch(SQLException e){
-			throw new DaoException(e.getMessage(),e);
+			statement = connection.prepareStatement(UPDATE_QUESTION_QUERY, question.getIdQuestion());
+			statement.setString(1, question.getEnonce());
+			statement.setString(2, question.getMedia());
+			statement.setLong(3, question.getPoints());
+			statement.setInt(4, question.getTheme());
+
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			throw new DaoException(e.getMessage(), e);
+		} finally {
+			ResourceUtil.safeClose(resultSet, statement, connection);
 		}
-		finally{
-			ResourceUtil.safeClose(resultSet,statement, connection)
-		}
-		
+
 	}
 
 	@Override
 	public void delete(Integer id) throws DaoException {
-		Connection connection= null;
+		Connection connection = null;
 		PreparedStatement statement = null;
-        ResultSet resultSet = null;
-        try{
-        	connection = MSSQLConnectionFactory.get();
-        	statement = connection.prepareStatement(DELETE_QUESTION_QUERY);
-        	statement.setInt(1, id);
-        	statement.executeUpdate();
-        	}
-        catch(SQLException e){
-        	throw new DaoException(e.getMessage(),e);
-        }
-        finally{
-        	ResourceUtil.safeClose(resultSet, statement, connection);
-        }
-        
+		ResultSet resultSet = null;
+		try {
+			connection = MSSQLConnectionFactory.get();
+			statement = connection.prepareStatement(DELETE_QUESTION_QUERY);
+			statement.setInt(1, id);
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			throw new DaoException(e.getMessage(), e);
+		} finally {
+			ResourceUtil.safeClose(resultSet, statement, connection);
+		}
+
 	}
 
 	@Override
 	public Question selectById(Integer id) throws DaoException {
-		// TODO Auto-generated method stub
-		return null;
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		Question question = null;
+		List<Proposition> listePropositions= new ArrayList();
+		try {
+			connection = MSSQLConnectionFactory.get();
+			statement = connection.prepareStatement(SELECT_QUESTION_QUERY);
+			statement.setInt(1, id);
+
+			resultSet = statement.executeQuery();
+
+			if (resultSet.next()) {
+				question = new Question();
+				question.setEnonce(resultSet.getString("enonce"));
+				question.setIdQuestion(id);
+				question.setMedia(resultSet.getString("media"));
+				question.setPoints(resultSet.getLong("points"));
+				question.setTheme(resultSet.getInt("idTheme"));
+				listePropositions = DaoFactory.propositionDAO().listePropositionsParQuestion(id);
+				question.setListePropositions(listePropositions);
+			
+			}
+
+		} catch (SQLException e) {
+			throw new DaoException(e.getMessage(), e);
+		} finally {
+			ResourceUtil.safeClose(resultSet, statement, connection);
+		}
+
+		return question;
+
 	}
 
 	@Override
 	public List<Question> selectAll() throws DaoException {
-		// TODO Auto-generated method stub
-		return null;
+		List<Question> listeQuestions = new ArrayList<>();
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
+		Integer questionId = null;
+		Question question = null;
+		List<Proposition> listePropositions = new ArrayList();
+		
+		try {
+			connection = MSSQLConnectionFactory.get();
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery(SELECT_ALL_QUESTIONS);
+
+			while (resultSet.next()) {
+				question = new Question();
+				question.setEnonce(resultSet.getString("enonce"));
+				question.setIdQuestion(resultSet.getInt("idQuestion"));
+				question.setMedia(resultSet.getString("media"));
+				question.setPoints(resultSet.getLong("points"));
+				question.setTheme(resultSet.getInt("idTheme"));
+				listePropositions = DaoFactory.propositionDAO().listePropositionsParQuestion(question.getIdQuestion());
+				question.setListePropositions(listePropositions);
+				listeQuestions.add(question);
+			}
+		} catch (SQLException e) {
+			throw new DaoException(e.getMessage(), e);
+		} finally {
+			ResourceUtil.safeClose(resultSet, statement, connection);
+		}
+
+		return listeQuestions;
 	}
 
 }
