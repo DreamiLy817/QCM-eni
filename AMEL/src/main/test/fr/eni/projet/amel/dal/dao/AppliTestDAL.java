e8c2ba42ce83c86254447b393553eb2a9040bafd
Leslie Baltimore <baltimore.leslie@gmail.com>
2018-10-10 09:42:01 +0200

    test

--- AMEL/src/main/test/fr/eni/projet/amel/dal/dao/AppliTestDAL.java
+++ AMEL/src/main/test/fr/eni/projet/amel/dal/dao/AppliTestDAL.java
@@ -1,18 +1,8 @@
 package fr.eni.projet.amel.dal.dao;
 
-import fr.eni.projet.amel.bo.Test;
-import fr.eni.projet.amel.dal.factory.DaoFactory;
-
 public class AppliTestDAL {
 	public static void main(String[] args) {
-		// declaration et instanciation de la DAO
 		
-		TestDao testDAO = DaoFactory.getTestDao();
-		
-		// Creer un nouveau test 
-		Test test1 = new Test("JAVA","test sur le language de programmation JAVA", 60, 80, 20);
-		System.out.println(test1);
 		
 	}
-	
 }
